7c6109fe798c8e3716aa8c83c0edfa1c292a139b
Sergio <sergio@panaghia.it>
2011-03-25 17:40:20 +0100

    IE styling weird bug fix

--- Source/Element.crossfade.js
+++ Source/Element.crossfade.js
@@ -14,30 +14,33 @@ provides: [Element.crossfade]
 ...
 */  
 
+
 Element.implement({
 	crossfade: function(to)
 	{
-		var cover = new Element('div', {
+		var cover = new Element('div', {   		
 			styles:{
 				position:'absolute',
 		 		top:'0px',
 		 		left:'0px',
 		 		right:'0px',
 		 		bottom:'0px',
+				width: this.getSize().x, //because of IE bug
+				height: this.getSize().y,//same
 		 		'background-image': 'url( '+to+')',
-				opacity: 0
+				'opacity': 0,
+				'z-index':1
 			}                                        	   
 		}); 			
-		cover.inject(this);   				
-		
+		cover.inject(this); 
+		   	
 		var fx = new Fx.Tween(cover, this.get('tween').options);
 		fx.addEvent('onComplete', function()
 		{
 		   	this.setStyle('background-image', 'url('+to+')');
 			cover.destroy(); 
 		}.bind(this)); 
-		fx.start('opacity', 1);   
-	}
-	
-})
+		fx.start('opacity', 1);		  
+	}	
+});
 
